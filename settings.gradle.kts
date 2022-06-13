29f240dba53a2d5b5ed4460803dbd6af389fdcc4
Nicolas <nicolas.bole@hotmail.com>
2022-06-13 13:08:44 -0300

    wip

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FinalSpace"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/finalspace/Greeting.kt
@@ -0,0 +1,7 @@
+package com.example.finalspace
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
